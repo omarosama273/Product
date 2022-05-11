4c4a1368e710a6d94c0cfb087f09a854fe5a3df6
omarosama273 <omarosama273273@gmail.com>
2022-05-12 01:52:49 +0200

    retrieve commit

--- js/main.js
+++ js/main.js
@@ -7,15 +7,23 @@ var productPriceInput = document.getElementById('productPrice')
 var productCategoryInput = document.getElementById('productCategory')
 var productDescInput = document.getElementById('productdDesc')
 var addBtn = document.getElementById('addBtn')
+var inputs = document.getElementsByClassName('form-control')
+
 var products = [];
+if(  JSON.parse( localStorage.getItem('productList'))){
+    products=  JSON.parse( localStorage.getItem('productList'))
+}
+displayData()
 
 addBtn.onclick = function () {
     addProduct();
-    displayData()
+    displayData();
+    resetForm()
+   
 }
 
 
-function addProduct(){
+function addProduct() {
 
     var product =
     {
@@ -25,13 +33,36 @@ function addProduct(){
         desc: productDescInput.value
     }
     products.push(product)
+    localStorage.setItem('productList',JSON.stringify(products))
 }
 
-function displayData(){
+function displayData() {
     var cartona = '';
 
     for (var i = 0; i < products.length; i++) {
-        cartona += '<tr><td>'+products[i].name+'</td><td>'+products[i].price+'</td><td>'+products[i].category+'</td><td>'+products[i].desc+'</td></tr>'
+        cartona += `<tr>
+        <td>${products[i].name}</td>
+        <td>${products[i].price}</td>
+        <td>${products[i].category}</td>
+        <td>${products[i].desc}</td>
+        <td><button class='btn btn-outline-warning'>update</button></td>
+        <td><button onclick="delateProduct(${i})" class='btn btn-outline-danger'>delate</button></td>
+        </tr>`;
     }
-   document.getElementById('tableBody').innerHTML=cartona
+
+    
+    document.getElementById('tableBody').innerHTML = cartona
+    
+}
+function delateProduct(index){
+   products.splice(index,1);
+   localStorage.setItem('productList',JSON.stringify(products))
+   displayData()
+}
+
+function resetForm() {
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].value = ''
+    }
+
 }
